4227f16246500ecb176b98010e5b33c0947014bc
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-01-29 15:37:18 +0100

    day and date update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
+// day and date update
+function displayDate(fulldate) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let numbermonth = fulldate.getMonth();
+  let month = months[numbermonth];
+  let date = fulldate.getDate();
+  let year = fulldate.getFullYear();
 
+  let sentencedate = `${date} ${month} ${year}`;
+  return sentencedate;
+}
+function displayWeekDay(fulldate) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let numberday = fulldate.getDay();
+  let day = weekdays[numberday];
+  //let hour = fulldate.getHours();
+  //let min = fulldate.getMinutes();
+  let sentenceDay = `${day}`;
+  return sentenceDay;
+}
+let now = new Date();
+document.querySelector("#weekday").innerHTML = displayWeekDay(now);
+document.querySelector("#date").innerHTML = displayDate(now);
